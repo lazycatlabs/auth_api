3b93b6a80d355879b7727330db5ab1f3a4058710
Lzyct <hey.mudassir@gmail.com>
2023-09-22 21:16:23 +0800

    feat: init project, create healthchecker

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use actix_web::{get, HttpResponse, HttpServer, Responder};
+
+use crate::models::response::Diagnostic;
+
+mod models;
+
+#[get("/api/healthchecker")]
+async fn healthchecker() -> impl Responder {
+    const MESSAGE: &str = "Hello, Lzyct";
+
+    HttpResponse::Ok().json(Diagnostic::new("Ok", MESSAGE))
+}
+
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    let (server, port) = ("127.0.0.1", 8000);
+
+    println!("🚀Server started {}:{}", server, port);
+
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .service(healthchecker)
+    }).bind((server, port))?
+        .run()
+        .await
+}

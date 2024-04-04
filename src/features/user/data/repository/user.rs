80786080d63e97a465f2fb564fd96ad339549671
Lzyct <hey.mudassir@gmail.com>
2024-04-04 14:39:23 +0800

    chore: update cargo version

--- src/features/user/data/repository/user.rs
+++ src/features/user/data/repository/user.rs
@@ -1,26 +1,20 @@
 use chrono::Utc;
-use diesel::{ExpressionMethods, RunQueryDsl};
 use diesel::prelude::*;
+use diesel::{ExpressionMethods, RunQueryDsl};
 use uuid::Uuid;
 
 use crate::{
     core::{
         constants::MESSAGE_SUCCESS,
         error::APIError,
-        types::{
-            AppResult,
-            DBConn,
-        },
+        types::{AppResult, DBConn},
     },
     features::user::{
         data::models::user::User,
         domain::{
             entity::user::UserEntity,
             repository::user::IUserRepository,
-            usecase::dto::{
-                RegisterParams,
-                UpdateUserParams,
-            },
+            usecase::dto::{RegisterParams, UpdateUserParams},
         },
     },
     schema::users::{self, dsl::*},
@@ -48,26 +42,29 @@ impl IUserRepository for UserRepository {
         {
             Ok(_) => Ok(MESSAGE_SUCCESS.to_string()),
             Err(diesel::result::Error::DatabaseError(
-                    diesel::result::DatabaseErrorKind::UniqueViolation,
-                    _,
-                )) => Err(APIError::BadRequest { message: format!("Email '{}' already exists.", user.email) }),
-            Err(_) => Err(APIError::BadRequest { message: "MESSAGE_INTERNAL_ERROR".to_string() }),
+                diesel::result::DatabaseErrorKind::UniqueViolation,
+                _,
+            )) => Err(APIError::BadRequest {
+                message: format!("Email '{}' already exists.", user.email),
+            }),
+            Err(_) => Err(APIError::BadRequest {
+                message: "MESSAGE_INTERNAL_ERROR".to_string(),
+            }),
         }
     }
 
     fn find_user_by_id(&self, user_id: Uuid) -> AppResult<UserEntity> {
         match users::table
             .filter(id.eq(user_id))
-            .get_result::<User>(&mut self.source.get().unwrap()) {
-            Ok(user) => Ok(
-                UserEntity {
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    photo: user.photo,
-                    verified: user.verified,
-                }
-            ),
+            .get_result::<User>(&mut self.source.get().unwrap())
+        {
+            Ok(user) => Ok(UserEntity {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                photo: user.photo,
+                verified: user.verified,
+            }),
             Err(_) => Err(APIError::UserNotFoundError),
         }
     }
@@ -81,7 +78,8 @@ impl IUserRepository for UserRepository {
                     verified.eq(params.verified.unwrap_or(user.verified)),
                     updated_at.eq(Utc::now().naive_utc()),
                 ))
-                .get_result::<User>(&mut self.source.get().unwrap()) {
+                .get_result::<User>(&mut self.source.get().unwrap())
+            {
                 Ok(updated_user) => Ok(UserEntity {
                     id: updated_user.id,
                     name: updated_user.name,
@@ -89,7 +87,7 @@ impl IUserRepository for UserRepository {
                     photo: updated_user.photo,
                     verified: updated_user.verified,
                 }),
-                Err(_) => Err(APIError::InternalError)
+                Err(_) => Err(APIError::InternalError),
             }
         } else {
             Err(APIError::UserNotFoundError)
@@ -98,10 +96,12 @@ impl IUserRepository for UserRepository {
 
     fn delete(&self, user_id: Uuid) -> AppResult<String> {
         if let Ok(user) = self.find_user_by_id(user_id) {
-            match diesel::delete(users.find(user.id))
-                .execute(&mut self.source.get().unwrap()) {
-                Ok(_) => Ok(format!("User with email '{}' deleted successfully", user.email)),
-                Err(_) => Err(APIError::InternalError)
+            match diesel::delete(users.find(user.id)).execute(&mut self.source.get().unwrap()) {
+                Ok(_) => Ok(format!(
+                    "User with email '{}' deleted successfully",
+                    user.email
+                )),
+                Err(_) => Err(APIError::InternalError),
             }
         } else {
             Err(APIError::UserNotFoundError)

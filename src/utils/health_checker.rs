6e1e51c337ae83a7ff62143d3986e82f41ff1409
Lzyct <hey.mudassir@gmail.com>
2024-07-16 14:50:35 +0800

    clean: merge the use crate

--- src/utils/health_checker.rs
+++ src/utils/health_checker.rs
@@ -1,7 +1,8 @@
 use actix_web::HttpResponse;
 
-use crate::core::middlewares::general::GeneralMiddleware;
-use crate::core::{response::ResponseBody, types::AppResult};
+use crate::core::{
+    middlewares::general::GeneralMiddleware, response::ResponseBody, types::AppResult,
+};
 
 pub async fn health_checker(_: GeneralMiddleware) -> AppResult<HttpResponse> {
     Ok(ResponseBody::<()>::success(None).into())

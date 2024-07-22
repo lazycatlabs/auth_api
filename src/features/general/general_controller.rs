de9652fe3e92318a936be32e1e7d0ee1bfebb285
Lzyct <hey.mudassir@gmail.com>
2024-07-23 00:09:12 +0800

    feat: add validator on send email

--- src/features/general/general_controller.rs
+++ src/features/general/general_controller.rs
@@ -2,16 +2,21 @@ use actix_web::{
     web::{self, Json},
     HttpResponse,
 };
+use validator::Validate;
 
-use crate::utils::mail_sender::send_email;
-
-use super::domain::usecase::dto::SendEmailParams;
-use crate::core::{middlewares::state::AppState, response::ResponseBody, types::AppResult};
+use crate::{core::error::APIError, features::general::domain::usecase::dto::SendEmailParams};
+use crate::{
+    core::{middlewares::state::AppState, response::ResponseBody, types::AppResult},
+    utils::mail_sender::send_email,
+};
 
 pub async fn test_email(
     _: web::Data<AppState>,
     params: Json<SendEmailParams>,
 ) -> AppResult<HttpResponse> {
+    params.validate().map_err(|e| APIError::BadRequest {
+        message: e.to_string(),
+    })?;
     send_email(params.0)
         .await
         .map(|_| ResponseBody::<()>::success(None).into())

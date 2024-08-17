e7032e451b724813fe003596ad1cdc866057af57
Lzyct <hey.mudassir@gmail.com>
2024-08-18 00:31:11 +0800

    feat: add default photo

--- src/features/user/domain/usecase/dto.rs
+++ src/features/user/domain/usecase/dto.rs
@@ -26,6 +26,7 @@ camel_case_struct!(RegisterParams {
 
 impl From<RegisterParams> for User {
     fn from(params: RegisterParams) -> Self {
+        let default_photo = String::from("https://user-images.githubusercontent.com/1531684/281937715-f53c55be-4b70-43b5-bb50-11706fb71ada.png");
         User {
             id: Uuid::new_v4(),
             email: params.email.unwrap(),
@@ -34,7 +35,7 @@ impl From<RegisterParams> for User {
             role: String::from("user"),
             created_at: Utc::now().naive_utc(),
             updated_at: Utc::now().naive_utc(),
-            photo: params.photo.unwrap(),
+            photo: params.photo.unwrap_or(default_photo),
             verified: false,
         }
     }

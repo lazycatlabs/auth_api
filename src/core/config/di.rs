d1eb13de3ede8f489e5c2d73da8b507ee95152c6
Lzyct <hey.mudassir@gmail.com>
2024-08-21 00:03:31 +0800

    clean: remove unused files and code

--- src/core/config/di.rs
+++ src/core/config/di.rs
@@ -1,19 +1,13 @@
-use std::sync::Arc;
-
 use crate::{
     core::types::DBConn,
     features::{
         auth::data::repository::auth_repository_impl::AuthRepository,
-        user::{
-            data::repository::user_repository_impl::UserRepository,
-            domain::usecase::service::UserService,
-        },
+        user::data::repository::user_repository_impl::UserRepository,
     },
 };
 
 #[derive(Clone)]
 pub struct DiContainer {
-    pub user_service: UserService,
     pub auth_repository: AuthRepository,
     pub user_repository: UserRepository,
 }
@@ -22,13 +16,11 @@ impl DiContainer {
     pub fn new(db_conn: &DBConn) -> Self {
         // user
         let user_repository = UserRepository::new(db_conn.clone());
-        let user_service = UserService::new(Arc::new(user_repository.clone()));
 
         // auth
         let auth_repository = AuthRepository::new(db_conn.clone());
 
         Self {
-            user_service,
             auth_repository,
             user_repository,
         }
--- src/features/user/domain/usecase/mod.rs
+++ src/features/user/domain/usecase/mod.rs
@@ -1,7 +1,4 @@
 pub mod delete_user;
-pub mod dto;
-pub mod interface;
 pub mod list_user;
 pub mod register;
-pub mod service;
 pub mod update_user;
